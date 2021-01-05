6bb0a620e3d74836fdc1af7e5716a9e9c525a747
juthamart <admin_bc11@hotmail.com>
2021-01-05 22:18:34 +0700

    Updated  header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -7,7 +7,8 @@ export default () => {
   return (
     <BrowserRouter>
       <div>
-        <Header/>
+        {/* <Header/> */}
+        Test
         <MarketingApp />
       </div>
     </BrowserRouter>
