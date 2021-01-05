876a6033f1f68acfd000fcdaacb25c294e41d1a9
juthamart <admin_bc11@hotmail.com>
2021-01-06 01:11:17 +0700

    randomize production classnames

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { 
-    StylesProvider, 
+import {
+    StylesProvider,
     createGenerateClassName,
- } from '@material-ui/core/styles';
+} from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
